d595e413ec0687adfc1c6a7061a636d0c16d3828
PieterHartanto <138848689+PieterHartanto@users.noreply.github.com>
2026-02-20 10:28:44 +0700

    Perbaikan

--- Jobsheet2/Mahasiswa21.java
+++ Jobsheet2/Mahasiswa21.java
@@ -9,8 +9,8 @@ class Mahasiswa21 {
     void tampilkanInformasi(){
     System.out.println("Nama: " + nama);
     System.out.println("NIM: " + nim);
-    System.out.println("Kelas: " + kelas);
     System.out.println("IPK: " + ipk);
+    System.out.println("Kelas: " + kelas);
     }
 
     void ubahKelas(String kelasBaru){
